2d643771f964b09fbef5eff5a01c957bed40e615
Deshan Basura <deshanbasura3@gmail.com>
2019-11-23 04:31:23 -0500

    added welcome message

--- bot.py
+++ bot.py
@@ -8,12 +8,23 @@ client = discord.Client()
 @client.event
 async def on_message(messsage):
     id = client.get_guild(int(SERVER_ID))
+    channels = ["commands"]
     
-    # if messsage.content.lower().find("!hello") != -1:
-    #     await messsage.channel.send("Hi") 
-        
-    if messsage.content == "!users":
-        await messsage.channel.send(id.member_count)
+    if str(messsage.channel) in channels:
+        if messsage.content.lower().find("!hello") != -1:
+            await messsage.channel.send("Hi") 
+            
+        elif messsage.content == "!users":
+            await messsage.channel.send(id.member_count)
+
+@client.event
+async def on_member_join(message):
+    # id = client.get_guild(int(SERVER_ID))
+    # channels = ["commands"]
+    for channel in member.server.channels:
+        if str(channel) == "general":
+            await client.send_message(f"""Welcome to the server {member.mention}""")
+
 
 
 client.run(TOKEN)
